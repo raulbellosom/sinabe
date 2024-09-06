b962a050856614bdd9a2db6099113421c8512f4f
Raul Belloso Medina <raul.belloso.m@gmail.com>
2024-09-06 16:38:20 -0600

    Fix visuals and append new fields to vehicles 2

--- mycad-frontend/src/components/Table/TableHeader.jsx
+++ mycad-frontend/src/components/Table/TableHeader.jsx
@@ -2,7 +2,7 @@ import ActionButtons from '../ActionButtons/ActionButtons';
 
 const TableHeader = ({ title, actions }) => {
   return (
-    <div className="flex flex-col-reverse md:flex-row md:items-center md:justify-between text-center md:text-start bg-white gap-4">
+    <div className="flex flex-col-reverse md:flex-row md:items-center md:justify-between text-start bg-white gap-4">
       <h1 className="text-xl font-bold text-orange-500 w-full">{title}</h1>
       <div className="w-full flex gap-2 justify-start md:justify-end">
         <ActionButtons extraActions={actions} />
