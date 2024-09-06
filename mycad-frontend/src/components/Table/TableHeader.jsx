92839a9b0aa9166ddd5b70cac962f7f14124b1ca
Raul Belloso Medina <raul.belloso.m@gmail.com>
2024-09-06 16:31:44 -0600

    Fix visuals and append new fields to vehicles

--- mycad-frontend/src/components/Table/TableHeader.jsx
+++ mycad-frontend/src/components/Table/TableHeader.jsx
@@ -4,7 +4,7 @@ const TableHeader = ({ title, actions }) => {
   return (
     <div className="flex flex-col md:flex-row md:items-center md:justify-between text-center md:text-start bg-white gap-4">
       <h1 className="text-xl font-bold text-orange-500 w-full">{title}</h1>
-      <div className="w-full flex gap-4 md:gap-2 justify-center md:justify-end">
+      <div className="w-full flex gap-2 justify-start md:justify-end">
         <ActionButtons extraActions={actions} />
       </div>
     </div>
