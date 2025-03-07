2222873614b9357be02fe1f3aeb29f3499b1f009
Raul Belloso Medina <raul.belloso.m@gmail.com>
2025-03-07 16:26:44 -0600

    update Vite version, enhance query client options, and add index.html for improved app structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,14 @@ import AppRouter from './router/AppRouter';
 import { Toaster } from 'react-hot-toast';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5, // 5 minutos
+      cacheTime: 1000 * 60 * 10, // 10 minutos
+    },
+  },
+});
 
 function App() {
   return (
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
-    port: 5173,
-    watch: {
-      usePolling: true, // Usa polling para detectar cambios en archivos
-    },
-  },
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
+    host: true, // Permite acceder desde la red
+    port: 5173, // Asegura que está en el puerto correcto
+    strictPort: true,
   },
 });
