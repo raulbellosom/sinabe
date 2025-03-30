9c04c060fb3a3a340b8777faa7e472534f047828
Raul Belloso Medina <raul.belloso.m@gmail.com>
2025-03-30 11:27:30 -0600

    feat: enhance SerialNumberField with error handling and improved UI feedback

--- frontend/src/components/Inputs/SerialNumberField.jsx
+++ frontend/src/components/Inputs/SerialNumberField.jsx
@@ -1,14 +1,19 @@
-// SerialNumberField.jsx
-import React from 'react';
-import { useField, useFormikContext } from 'formik';
+import React, { useState } from 'react';
+import { useFormikContext } from 'formik';
 import { checkSerialNumber } from '../../services/api';
-import TextInput from './TextInput';
+import { ErrorMessage } from 'formik';
+import { TextInput as Input, Label } from 'flowbite-react';
 import classNames from 'classnames';
 
-const SerialNumberField = ({ inventoryId, ...props }) => {
-  const [field] = useField(props?.field?.name);
+const SerialNumberField = ({
+  className,
+  field,
+  form: { touched, errors },
+  inventoryId,
+  ...props
+}) => {
   const { setFieldError } = useFormikContext();
-
+  const [error, setError] = useState(null);
   const handleBlur = async (e) => {
     field.onBlur(e);
     const serial = e.target.value;
@@ -16,9 +21,11 @@ const SerialNumberField = ({ inventoryId, ...props }) => {
     if (serial) {
       try {
         const result = await checkSerialNumber(serial, inventoryId);
-        console.log(result);
         if (result.exists) {
-          setFieldError(props?.field?.name, 'El número de serie ya existe');
+          setFieldError(field.name, 'El número de serie ya existe');
+          setError('El número de serie ya existe');
+        } else {
+          setError(null);
         }
       } catch (error) {
         console.error('Error al validar el serial:', error);
@@ -26,7 +33,37 @@ const SerialNumberField = ({ inventoryId, ...props }) => {
     }
   };
 
-  return <TextInput {...field} {...props} onBlur={handleBlur} />;
+  return (
+    <div className={classNames('relative w-full', className)}>
+      <Label
+        htmlFor={props.id || props.name}
+        className={'block text-sm font-medium'}
+        color={touched[field?.name] && errors[field?.name] ? 'failure' : ''}
+        value={props.label}
+      />
+      <div className="relative">
+        <Input
+          {...field}
+          {...props}
+          color={
+            (touched[field?.name] && errors[field?.name]) || error
+              ? 'failure'
+              : ''
+          }
+          className="mt-1 text-neutral-800"
+          onBlur={handleBlur}
+        />
+      </div>
+      <div className="text-red-500 text-sm">
+        <ErrorMessage
+          name={field?.name}
+          component="div"
+          className="text-red-500 text-sm"
+        />
+        {error && <div className="text-red-500 text-sm">{error}</div>}
+      </div>
+    </div>
+  );
 };
 
 export default React.memo(SerialNumberField);
